b52477a558b78f99b2cc9bf4036b5dfed6af63ec
monaghaa <33913248+monaghaa@users.noreply.github.com>
2020-10-14 09:19:52 -0600

    Update submit_hostname.sh

--- submit_hostname.sh
+++ submit_hostname.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --nodes=1			# Number of requested nodes
-#SBATCH --ntasks=1			# Number of requested cores
-#SBATCH --time=00:01:00		# Max wall time
-#SBATCH --qos=blanca-papp	# Specify QOS; may be just “blanca-appm” for some
-#SBATCH --partition=blanca-papp 		# Specify APPM nodes
-#SBATCH --account=blanca-papp 		# Specify account
-#SBATCH --output=hostname_%j.out		# Rename standard output file
-#SBATCH --job-name=hostname		# Job submission name
+#SBATCH --nodes=1			           # Number of requested nodes
+#SBATCH --ntasks=1			         # Number of requested cores
+#SBATCH --time=00:01:00		       # Max wall time
+#SBATCH --qos=blanca-papp	       # Specify QOS
+#SBATCH --partition=blanca-papp  # Specify nodes
+#SBATCH --account=blanca-papp 	 # Specify account
+#SBATCH --output=hostname_%j.out # Rename standard output file
+#SBATCH --job-name=hostname		   # Job submission name
 
 # purge all existing modules
 module purge
